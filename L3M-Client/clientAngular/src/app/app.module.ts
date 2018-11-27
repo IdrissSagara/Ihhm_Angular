081b0196700b75416a7c527c41a29f37316c2929
Sagara Idriss <sagara.idriss@gmail.com>
2018-11-27 10:57:57 +0100

    Affichage des données ok

--- L3M-Client/clientAngular/src/app/app.module.ts
+++ L3M-Client/clientAngular/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SecretaryComponent } from './secretary/secretary.component';
+import { CabinetComponent } from './cabinet/cabinet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecretaryComponent
+    SecretaryComponent,
+    CabinetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
